366e71e4fcd213fe546fb28b7bd1f161f482b4c7
Akash S <akash.shingte@gmail.com>
2018-10-12 11:25:58 -0400

    Initial commit

---
+++ lib/chord.ex
@@ -0,0 +1,25 @@
+defmodule Chord do
+  @moduledoc """
+  Documentation for Chord.
+  """
+  use Application
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Chord.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+
+  @doc """
+  Start the Application
+  """
+  def start() do
+  end
+end
